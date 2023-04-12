22cab78019d73ae2a9d8d0c65f6031554b302a97
pun33t19 <puneet22088@gmail.com>
2023-04-13 01:21:51 +0530

    api json change

--- index.js
+++ index.js
@@ -4,4 +4,145 @@ const app = express();
 
 app.listen(5001, () => console.log('API running'))
 
-app.get('/', (req, res) => res.json('My Api Running'))
+const jsonSample = [{
+    "Posts": {
+        "1674117237670": {
+            "pDescription": "So, why bother to learn to deadlift? Quite simply, it's one of the most effective exercises for developing the pure strength that leads to bodily size and athleticism. Since it's a full-body movement that uses a lot of muscle mass, the deadlift also builds total-body muscle.1\n\nIt's one of the few lifts that directly targets the hamstrings, a group of muscles often overlooked in the weight room.2 It also works your glutes, lats (upper back), and core.\n\nThis magic lift also improves posture. We live most of our lives focusing on the front of our bodies, ignoring our rears. In turn, we develop bodily frames without balance, leading to a host of postural issues—hunched shoulders and weak backs, for example.\n\nDeadlifting reintroduces us to our body's backside—the hamstrings, glutes, and back. Posterior training balances the body, helping us stand taller and with greater strength.\n\nIn short, deadlifting will build muscle, improve your posture, give you balanced, full-body strength, and turn you into a total gym badass. After all, there's nothing quite like ripping heavy weight from the ground.\n\nHow To Fit Deadlifts Into Your Workout\nDeadlifting makes the body pay a heavy tax—the nervous system and the musculoskeletal system both work hard. Since it can drain the nervous system, it's best for a beginner to train it at the beginning of a workout.\n\nA fresh nervous system means productive reps, because the body more efficiently learns movement and you'll have better form. It's also safer. As you get tired, your form gets worse and injury is more likely. It's best to plan deadlift training for the time period right after your warm-up.\n\nDon't think of this workout like you would a traditional body part, like back or chest. Instead, you'll work a number of important assistance exercises with it.\n\nWhen you do a deadlift workout, beware of training any of the same muscle groups the day before or the day after. Proper recovery allows for gains in both strength and size.\n\n",
+            "pId": "1674117237670",
+            "pImage": "https://firebasestorage.googleapis.com/v0/b/csiapp-9abb7.appspot.com/o/Posts%2Fpost_1674117237670?alt=media&token=995d631d-2ea0-4912-9ff6-937b0508689e",
+            "pTime": "1674117237670",
+            "pTitle": "How to Deadlift for Beginners ",
+            "uEmail": "umang@gmail.com",
+            "uid": "fMxncwNMs1f3LbelZo8eM61OuVd2"
+        },
+        "1674117432287": {
+            "pDescription": "The other day, one of my weight-loss clients told me that she’d been “starving” while waiting for lunchtime to come around.\n\nOf course, she didn’t mean that she was literally starving. She was just trying to say she was feeling “especially hungry.”\n\nBut I always discourage people I work with from using that word, because — aside from being insensitive to the fact that lots of people really ARE starving — it just creates a terrible mindset for anyone who’s trying to eat purposefully and lose weight.\n\nWhenever you say that you’re starving (even in jest), some part of you begins to panic. And that feeling of panic can lead to bad decisions — like choosing to eat the next thing in sight.\n\nAnd when the next thing in sight is a bag of chips or a sugary snack — that’s a choice that won’t help you reach your weight-loss goals and probably won’t even alleviate your hunger for long.\n\nSo before switching into panic mode, I advise my clients to take a breath — and instead of saying “I’m starving” say “I’m okay. I’m just hungry and I need to eat something.”\n\nI always tell them to start by drinking water first (because hunger is very often dehydration in disguise!) and to remind themselves of WHY they’re trying to lose weight.\n\nAfter that, it takes them just a few extra minutes to think of a healthy, filling option that will satisfy their hunger, nourish their bodies, and still help them reach their weight-loss goals.\n\nRemember, you’re always in control of the food you eat — that’s an amazingly liberating idea!\n\nLearn to eat because you’re hungry — not because you’re “starving” — and you’ll take a giant step down the path to a positive weight loss and mindset.\n\nBe sure to try my new nutrition program — the 2B Mindset — available now on TeamBeachbody.com.\n\nIt’s a healthy approach to weight loss that shows you how to coexist with food in real-world situations, outsmart your cravings, and manage setbacks before they even occur.\n\nBest of all, it is designed to help you feel full and ",
+            "pId": "1674117432287",
+            "pImage": "https://firebasestorage.googleapis.com/v0/b/csiapp-9abb7.appspot.com/o/Posts%2Fpost_1674117432287?alt=media&token=4affd7e6-816c-472a-8f48-5e39a0a05930",
+            "pTime": "1674117432287",
+            "pTitle": "You CAN lose weight without \"Starving\"",
+            "uEmail": "umang@gmail.com",
+            "uid": "fMxncwNMs1f3LbelZo8eM61OuVd2"
+        },
+        "1674117553761": {
+            "pDescription": "1. Eat more plants\nThe key here is what Dr. Barbara Rolls termed “volumetrics.” Foods with low-calorie density like fruits and veggies are naturally high in bulk, fiber, and water to help you feel full without using up much of your daily calorie target.\n\nIf you think about it, when was the last time you were able to eat more than two apples in a sitting or come close to eating a whole head of lettuce?\n\n(Juicing doesn’t count — once you send those fruits and veggies through the juicer, all the beneficial fiber is removed.) The more of these foods you have in your meal plan, the less likely you’ll be to binge on calorie-dense treats.\n\n2. Cook (more) at home\nHopefully, you got some practice from following the recipes in the nutrition guide that accompanied your favorite Beachbody On Demand program.\n\nNow it’s time to take control and perhaps even kick things up a notch. With the advent of that Internet thing, you have access to loads of healthful recipes — like the ones on the Beachbody Blog and Autumn Calabrese’s cooking show, FIXATE.\n\nSo whether you consider yourself a foodie or not, seek out blogs that feature recipes that suit your taste.\n\nWhen you cook at home, you control your portions, the quality of the ingredients, the cooking method, and the menu — all areas that require wise decisions when it comes to losing weight and keeping it off.",
+            "pId": "1674117553761",
+            "pImage": "https://firebasestorage.googleapis.com/v0/b/csiapp-9abb7.appspot.com/o/Posts%2Fpost_1674117553761?alt=media&token=208c86ab-17ef-450e-96e3-e4ded27d441a",
+            "pTime": "1674117553761",
+            "pTitle": "Tips for Maintaining weight loss",
+            "uEmail": "umang@gmail.com",
+            "uid": "fMxncwNMs1f3LbelZo8eM61OuVd2"
+        },
+        "1674117854841": {
+            "pDescription": "There’s an equal amount of confusion and hype surrounding a low-carb diet.\n\nResearch shows low-carb diets can be an effective way to shed pounds — although not necessarily superior to weight-reduction results achieved by other diets, such as low-fat or reduced-calorie diets.\n\nBut a low-carb diet plan isn’t as straightforward as the name might have you believe.\n\n“A low-carbohydrate diet can have a wide, unclear definition,” says Holly Klamer, M.S., R.D., “but in general terms, it means following a diet that has less than 45–65 percent of [total daily] calories from carbohydrates.”\n\n(For reference, the recommended carbohydrate range for adults, according to the 2015–2020 Dietary Guidelines for Americans, is 45–65 percent of total daily calories.)\n\nSome low-carb diets, like the modern Atkins diet, for example, limits trans fat and sugar in addition to carbs, while a ketogenic diet drastically reduces carbs and replaces them with fats.\n\nIn general, however, most low-carb diets focus on limiting refined grains and starches (like white bread, pasta, and potatoes) in favor of lean protein, whole grains, non-starchy veggies, and low-glycemic fruits.\n\nBut with so much varying information out there, it can be easy to misinterpret  low-carb diet or to implement its principles in an extreme or unsustainable way.\n\nThe intention behind the diet — to reduce the amount of unhealthy carbs you consume on a regular basis — isn’t inherently a bad idea, but you need to be smart about how you execute it.\n\nHere are seven common mistakes to avoid.",
+            "pId": "1674117854841",
+            "pImage": "https://firebasestorage.googleapis.com/v0/b/csiapp-9abb7.appspot.com/o/Posts%2Fpost_1674117854841?alt=media&token=0e7cd490-89d0-4700-ace8-48f87eb3bcba",
+            "pTime": "1674117854841",
+            "pTitle": "7 Common low-carb diet Mistakes",
+            "uEmail": "umang@gmail.com",
+            "uid": "fMxncwNMs1f3LbelZo8eM61OuVd2"
+        },
+        "1674118072307": {
+            "pDescription": "Let’s face it: “Healthy eating” has a PR problem.\n\nIf you’re new to eating healthy, that phrase may bring to mind an unappetizing image of a boring plate of salad topped with grilled chicken next to a piece of fruit.\n\nIt’s what you might scoff at as “rabbit food.”\n\nYes, foods like fruits, veggies, protein, and whole grains are part of a balanced, healthy diet, but that doesn’t mean you’re stuck with plain oatmeal and a banana for breakfast every day.\n\n(Pro tip: A daily Shakeology is an easy, delicious way to jazz up your morning meal. Find your favorite recipe here!)\n\nWhole foods can act as a blank canvas for exciting flavors and textures.\n\nIt’s all about targeting your senses by maximizing the deliciousness of your healthy meals, so you’ll want to eat them again and again.\n\nFirst, let’s explore these senses and how they affect your eating experience.\n\nHealthy eating tips, meal prep, portion fix, healthy eating\n\nHow Your Senses Affect Your Eating Experience\nEating is all about the senses. Taste, smell, sight, touch, and even sound all play into our enjoyment of food.\n\nTaste\nTaste is crucial to appreciating food. You have 10,000 taste buds that are evolutionarily wired to like sweet, salty, and rich (aka, fatty) foods because they contain lots of calories and nutrients compatible with survival.\n\nA study by Yale researchers found that mice genetically bred without the ability to taste sweetness don’t prefer sugar water any more than plain water at first.\n\nAfter a few days, they learned to prefer the sugar water, likely because it contained a prized reward: calories.\n\nDrinking the sugar water also prompted the release of dopamine, which plays a major role in the reward-motivated behavior of the brain.\n\nTrue, humans are more complex than mice, but this experiment indicates it’s possible for taste preferences to be rewired.\n\nSmell\nSmell works closely with taste to create flavor. There are five tastes (sweet, salty, sour, bitter, umami), but there are thousands of smells ",
+            "pId": "1674118072307",
+            "pImage": "https://firebasestorage.googleapis.com/v0/b/csiapp-9abb7.appspot.com/o/Posts%2Fpost_1674118072307?alt=media&token=70543757-88cf-407a-8e63-5e59812183d6",
+            "pTime": "1674118072307",
+            "pTitle": "8 Healthy Eating tips",
+            "uEmail": "umang@gmail.com",
+            "uid": "fMxncwNMs1f3LbelZo8eM61OuVd2"
+        },
+        "1674201974222": {
+            "pDescription": "Anahah",
+            "pId": "1674201974222",
+            "pImage": "https://firebasestorage.googleapis.com/v0/b/csiapp-9abb7.appspot.com/o/Posts%2Fpost_1674201974222?alt=media&token=1c91a46f-e889-4535-beb5-bec9bf708f84",
+            "pTime": "1674201974222",
+            "pTitle": "ahah",
+            "uEmail": "singhaniruddh30@gmail.com",
+            "uid": "RdmyWLLvslOrA9IjdTaRk4bJPwj2"
+        },
+        "1674307470782": {
+            "pDescription": "Dj",
+            "pId": "1674307470782",
+            "pImage": "https://firebasestorage.googleapis.com/v0/b/csiapp-9abb7.appspot.com/o/Posts%2Fpost_1674307470782?alt=media&token=42991d82-fce0-47e1-89c3-7e1d59b943f7",
+            "pTime": "1674307470782",
+            "pTitle": "my new blog",
+            "uEmail": "rajkamal@gmail.com",
+            "uid": "FzNMv1Prh8Spx6c9Otx8CT824RA2"
+        },
+        "1674307481728": {
+            "pDescription": "Dj",
+            "pId": "1674307481728",
+            "pImage": "https://firebasestorage.googleapis.com/v0/b/csiapp-9abb7.appspot.com/o/Posts%2Fpost_1674307481728?alt=media&token=358aec5b-7668-4c18-826a-0a61b0feeb39",
+            "pTime": "1674307481728",
+            "pTitle": "my new blog",
+            "uEmail": "rajkamal@gmail.com",
+            "uid": "FzNMv1Prh8Spx6c9Otx8CT824RA2"
+        },
+        "1674328739565": {
+            "pDescription": "Gsosi",
+            "pId": "1674328739565",
+            "pImage": "https://firebasestorage.googleapis.com/v0/b/csiapp-9abb7.appspot.com/o/Posts%2Fpost_1674328739565?alt=media&token=5e960181-49bd-4e29-ab83-12ecde54b435",
+            "pTime": "1674328739565",
+            "pTitle": "tjsn",
+            "uEmail": "rajkamal@gmail.com",
+            "uid": "FzNMv1Prh8Spx6c9Otx8CT824RA2"
+        },
+        "1674469580093": {
+            "pDescription": "Snsjsjsi",
+            "pId": "1674469580093",
+            "pImage": "https://firebasestorage.googleapis.com/v0/b/csiapp-9abb7.appspot.com/o/Posts%2Fpost_1674469580093?alt=media&token=84892267-d75f-414a-bda7-3c4bab71f19b",
+            "pTime": "1674469580093",
+            "pTitle": "sjajaiia",
+            "uEmail": "singhaniruddh30@gmail.com",
+            "uid": "RdmyWLLvslOrA9IjdTaRk4bJPwj2"
+        },
+        "1674565371827": {
+            "pDescription": "Hxiaja",
+            "pId": "1674565371827",
+            "pImage": "https://firebasestorage.googleapis.com/v0/b/csiapp-9abb7.appspot.com/o/Posts%2Fpost_1674565371827?alt=media&token=1f1f7dec-5b4e-4c73-b662-24005097d363",
+            "pTime": "1674565371827",
+            "pTitle": "tsiabsywk",
+            "uEmail": "rajkamal@gmail.com",
+            "uid": "FzNMv1Prh8Spx6c9Otx8CT824RA2"
+        }
+    },
+    "UserData": {
+        "FzNMv1Prh8Spx6c9Otx8CT824RA2": {
+            "Target": "muscle gain",
+            "age": 20,
+            "bmi": "22.58",
+            "height": "181",
+            "uEmail": "rajkamal@gmail.com",
+            "uid": "FzNMv1Prh8Spx6c9Otx8CT824RA2",
+            "weight": "74"
+        },
+        "RdmyWLLvslOrA9IjdTaRk4bJPwj2": {
+            "Target": "muscle gain",
+            "age": 20,
+            "bmi": "26.37",
+            "height": "147",
+            "uEmail": "singhaniruddh30@gmail.com",
+            "uid": "RdmyWLLvslOrA9IjdTaRk4bJPwj2",
+            "weight": "57"
+        },
+        "fMxncwNMs1f3LbelZo8eM61OuVd2": {
+            "Target": "weight gain",
+            "age": 21,
+            "bmi": "23.12",
+            "height": "157",
+            "uEmail": "umang@gmail.com",
+            "uid": "fMxncwNMs1f3LbelZo8eM61OuVd2",
+            "weight": "57"
+        },
+        "oiz5nxL1Jqb46cgHwOyIiKDhcks1": {
+            "Target": "weight gain",
+            "age": 0,
+            "bmi": "24.30",
+            "height": "120",
+            "uEmail": "pun33t14@gmail.com",
+            "uid": "oiz5nxL1Jqb46cgHwOyIiKDhcks1",
+            "weight": "35"
+        }
+    }
+}]
+app.get('/', (req, res) => res.json(json))
